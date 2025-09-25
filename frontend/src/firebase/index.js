c0302c631d35d2a17fe3a81354c35ae4dbdee81b
Han gyeonghun <91012303+gyeongpunch@users.noreply.github.com>
2025-09-25 17:24:22 +0900

    Update index.js

--- frontend/src/firebase/index.js
+++ frontend/src/firebase/index.js
@@ -7,12 +7,6 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBA9AaXct048KqNs0QlGbIUBEq88dJUFN8",
-  authDomain: "areuhot-5d4dd.firebaseapp.com",
-  projectId: "areuhot-5d4dd",
-  storageBucket: "areuhot-5d4dd.firebasestorage.app",
-  messagingSenderId: "498943643581",
-  appId: "1:498943643581:web:a0e91ed803bafc24c28cd4",
 };
 
 // Initialize Firebase
